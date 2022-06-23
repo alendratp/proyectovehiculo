37b2efe577dacdecf4fa262b0f6bff8c0a7b6812
lennymunoz <lennymunoz63@gmail.com>
2022-06-23 10:36:17 -0400

    se crea acciones

--- src/main/java/acciones/Iacciones.java
+++ src/main/java/acciones/Iacciones.java
@@ -1,4 +1,10 @@
 package acciones;
 public interface Iacciones {
     
+     public abstract void Encender();
+     
+      public abstract void Frenar();
+      
+      
+      public abstract void Apagar();
 }
