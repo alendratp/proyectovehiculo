54eec868e83450fd4beed4682d62512f4187b4ba
cristofer <cristofercastro3btp@gmail.com>
2022-06-23 10:33:12 -0400

    Se guarda proyecto

--- src/main/java/clases/Avion.java
+++ src/main/java/clases/Avion.java
@@ -1,7 +1,32 @@
-
 package clases;
+import acciones.Acciones;
+ public class Avion extends Vehiculo implements Acciones{
+    private int Ocupantes;
 
+    public Avion(int Ocupantes) {
+        this.Ocupantes = Ocupantes;
+    }
 
-public class Avion {
-    
+    public int getOcupantes() {
+        return Ocupantes;
+    }
+
+    public void setOcupantes(int Ocupantes) {
+        this.Ocupantes = Ocupantes;
+    }
+
+    @Override
+    public void encender() {
+        System.out.println("");
+    }
+
+    @Override
+    public void frenar() {
+        System.out.println("");
+    }
+
+    @Override
+    public void apagar() {
+        System.out.println("");
+    } 
 }
--- src/main/java/clases/Camion.java
+++ src/main/java/clases/Camion.java
@@ -1,7 +1,27 @@
-
 package clases;
+import interfaces.Acciones;
+public class Camion extends Vehiculo implements Acciones{
+    private int pesoMaximo;
 
+    public Camion(int pesoMaximo, String nombre, String modelo, int anioFabricacion) {
+        super(nombre, modelo, anioFabricacion);
+        this.pesoMaximo = pesoMaximo;
+    }
 
-public class Camion {
+ 
+    public int getpesoMaximo() {
+        return pesoMaximo;
+    }
+
+    public void setpesoMaximo(int pesoMaximo) {
+        this.pesoMaximo = pesoMaximo;
+    }
     
+    
+    public void encender(){
+        System.out.println("estoy encendiendo el camion");
+    }
+    
+    public void frenar(){
+        System.out.println("estoy frenando el camion");
 }
